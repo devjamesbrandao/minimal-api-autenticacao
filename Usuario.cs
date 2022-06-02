162a6982c99b613b86429e26dbe6a9f7d6ae967c
James Noleto <muricijames@gmail.com>
2022-06-01 21:09:35 -0300

    feat: entidade para representar o usuário da aplicação

---
+++ Usuario.cs
@@ -0,0 +1,10 @@
+namespace Autenticacao_Identity
+{
+    public class Usuario
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+        public string Role { get; set; }
+    }
+}
